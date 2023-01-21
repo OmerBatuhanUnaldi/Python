b21a83756ea81eb74a43a32f895355982fa95d78
OmerBatuhanUnaldi <123231652+OmerBatuhanUnaldi@users.noreply.github.com>
2023-01-21 11:22:18 +0300

    Add files via upload

---
+++ Eti Karam Gurme Besin Değeri.py
@@ -0,0 +1,3 @@
+#Eti Karam Gurme'ye ait besin değerleri(50 gramında)
+print(" Ürünün adı: Eti Karam Gurme\n Ürünün içerdiği protein miktarı: 5.4 gram.\n Ürünün içerdiği yağ miktarı: 32.40 gram(Doymuş yağ 16.1 g, trans yağ 0.15 g).\n Ürünün içerdiği karbonhidrat miktarı: 56.7 gram.\n Ürünün içerdiği şeker miktarı: 37.1 gram.\n Ürünün içerdiği enerji: 2200 kilojoule / 527 kilokalori.\n Ürünün içerdiği lif miktarı: 4.3 gram.\n Ürünün içerdiği tuz miktarı: 0.4 gram.")
+#Bu üründeki karbonhidrat ve yağ miktarının fazla oluşu nedeniyle bu ürünün fazla tüketilmesi durumunda kilo artışı gözlenebilir.
